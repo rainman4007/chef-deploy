e69f86cbf8b15e49b40b32563736e5642701133d
root <root@ashok40071.mylabserver.com>
2017-04-23 07:12:02 +0000

    on going work on LA

---
+++ cookbooks/localusers/recipes/default.rb
@@ -0,0 +1,21 @@
+#
+# Cookbook Name:: localusers
+# Recipe:: default
+#
+# Copyright 2017, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+search(:users, "*:*").each do |data| 
+	user data["id"] do
+	comment data["comment"]
+	uid data["uid"]
+	gid data["gid"]
+	home data["home"]
+	shell data["shell"]
+	end
+
+end
+include_recipe "localusers::groups"
+
+
